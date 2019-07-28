9c6d2d990fb27be047861b5e9647af455e05bea8
arijit sahoo <tijira1999sahoo@gmail.com>
2019-07-28 21:29:46 +0530

    issues while fetching json requests

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
+const User = require('../model/User');
+
 
 router.post('/register', (req,res) => {
-    res.send('Register');
+    // res.send('Register');
+
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
 
 });
 
