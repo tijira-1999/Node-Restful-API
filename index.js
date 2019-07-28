d4d698d8d6c768a3f54d4df5ecb40431c7334383
arijit sahoo <tijira1999sahoo@gmail.com>
2019-07-28 17:34:52 +0530

    server setup with postman API environment

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
 
+//importing routes
+const authRoute = require('./routes/auth');
+
+// routes Middlewares
+app.use('/api/user',authRoute);
 
 app.listen(3000, () => console.log('server running'));
---
+++ routes/auth.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+router.post('/register', (req,res) => {
+    res.send('Rgeister');
+
+});
+
+
+module.exports = router;
